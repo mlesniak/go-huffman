924a4d53428b2d13cca8c7553e40209816e76116
Michael Lesniak <mlesniak@micromata.de>
2018-10-04 05:45:10 +0200

    Count number of char occurrences in a string

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Huffman encoding and decoding")
+	s := "Huffman encoding and decoding"
+	m := count(s)
+	fmt.Println(m)
+}
+
+func count(s string) map[string]float32 {
+	m := make(map[string]float32)
+
+	for _, v := range(s) {
+		m[string(v)]++
+	}
+
+	return m
 }
