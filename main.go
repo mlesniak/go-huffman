ed16a64f49e8736d295e28574145d28c1819524c
Michael Lesniak <mlesniak@micromata.de>
2018-10-04 05:51:22 +0200

    Use []byte instead of string

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
+// TODO Write unit test
 package main
 
 import "fmt"
 
 func main() {
 	s := "Huffman encoding and decoding"
-	m := count(s)
+	m := computeFrequency([]byte(s))
 	fmt.Println(m)
 }
 
-func count(s string) map[string]float32 {
-	m := make(map[string]float32)
+func computeFrequency(s []byte) map[byte]float32 {
+	m := make(map[byte]float32)
 
+	// Count absolute number.
 	for _, v := range(s) {
-		m[string(v)]++
+		m[v]++
 	}
 	
+	// Compute relative number of occurrences.
 	for k, v := range(m) {
 		m[k] = v / float32(len(s))
 	}
