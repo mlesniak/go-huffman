57ada73eac023711931daf4ec9ef6b6947832a2a
Michael Lesniak <mlesniak@micromata.de>
2018-10-12 05:55:42 +0200

    Add plan for next steps

--- main.go
+++ main.go
@@ -9,5 +9,13 @@ func main() {
 	m := NewHuffmanTree([]byte(s))
 	fmt.Println(m)
 	codebook := m.GetCodebook()
+
+	// In a file compression.go
+	// Find out how to write bits to file
+	// Write and think about header
+	// Write header to file
+	// Write codebook to file
+	// Encode contents
+
 	fmt.Println(codebook)
 }
