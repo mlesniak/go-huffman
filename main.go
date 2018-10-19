eafed806ff013fc98d00d9fb52cc26b3b14b7826
Michael Lesniak <mlesniak@micromata.de>
2018-10-19 21:18:53 +0200

    Prepare codebook storage

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
@@ -14,15 +13,24 @@ func main() {
 	m := NewHuffmanTree([]byte(s))
 	//fmt.Println(m)
 	codebook := m.GetCodebook()
-	fmt.Println(codebook)
+
+	//file, _ := os.Create("out.bit")
+
+	// byte, 3 bit for length, bit for codebook
+	for byteValue, code := range codebook {
+		encLen := intToBinary(len(code))
+		for len(encLen) < 3 {
+			encLen = append([]int{0}, encLen...)
+		}
+		fmt.Println(byteValue, encLen, code)
+	}
 
 	// $ xxd -b out.bit
 	// 00000000: 11111111
-	bits := []int{
-		1, 0, 1, 0, 0, 1, 1, 0,
-		1, 0, 0, 0, 0, 1}
-	file, _ := os.Create("out.bit")
-	WriteBits(file, bits)
+	//bits := []int{
+	//	1, 0, 1, 0, 0, 1, 1, 0,
+	//	1, 0, 0, 0, 0, 1}
+	//WriteBits(file, bits)
 
 	// In a file compression.go
 	// Find out how to write bits to file
@@ -31,4 +39,19 @@ func main() {
 	// Write codebook to file
 	// Encode contents
 
+}
+
+func intToBinary(value int) []int {
+	buffer := make([]int, 0)
+
+	for {
+		if value == 0 {
+			buffer = append(buffer, value&1)
+			break
+		}
+		buffer = append(buffer, value&1)
+		value = value >> 1
+	}
+
+	return buffer
 }
